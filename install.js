c12ab8cfee3e0bb655c2f82c20304f411b858a02
albertosouza <alberto.souza.99@gmail.com>
2016-06-21 22:21:18 -0300

    add install function to check if current project have any theme

---
+++ install.js
@@ -0,0 +1,16 @@
+module.exports = {
+  install: function install (we, done) {
+    if (we.config.themes && we.config.themes.enabled && we.config.themes.enabled.length) {
+      return done();
+    }
+
+    we.log.warn(
+      'No theme found, add one theme with commands\n'+
+      'we i we-theme-cluster we-plugin-bootstrap3\n'+
+      'we vset themes.enabled[0] we-theme-cluster\n'+
+      'we vset themes.app we-theme-cluster'
+    )
+
+    done()
+  }
+}
