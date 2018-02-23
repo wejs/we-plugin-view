0d784e23061b93f8a689218b53914698f727653b
albertosouza <alberto.souza.99@gmail.com>
2018-02-23 12:38:09 -0300

    improve theme routes access

--- plugin.js
+++ plugin.js
@@ -20,157 +20,6 @@ module.exports = function loadPlugin(projectPath, Plugin) {
       index(req, res) {
         res.locals.template = 'home/index';
         res.ok();
-      },
-
-      findThemes(req, res) {
-        const we = req.we;
-
-        res.locals.themes = we.view.themes;
-        res.locals.themeConfigs = we.config.themes;
-
-        res.ok();
-      },
-
-      getAllThemes(req, res) {
-        const we = req.we;
-
-        res.locals.themes = we.view.themes;
-        res.locals.themeConfigs = we.config.themes;
-
-        let themes = {};
-
-        for(let name in res.locals.themes) {
-          if (!res.locals.themes[name].description) continue;
-
-          themes[name] = {
-            name: name,
-            imageThumbnail: res.locals.themes[name].imageThumbnail,
-            imageLarge: res.locals.themes[name].imageLarge,
-            description: res.locals.themes[name].description,
-            configs: res.locals.themes[name].configs
-          };
-        }
-
-        let firstThemeName;
-        if (res.locals.themes && res.locals.themes[0]) {
-          firstThemeName = res.locals.themes[0].name;
-        }
-
-        res.ok({
-          themes: themes,
-          enabled: we.systemSettings.siteTheme || firstThemeName
-        });
-      },
-
-      installTheme(req, res, next) {
-        const we = req.we;
-        // req.params.name
-        // req.body.colorName
-        // req.body.release
-
-        if (!req.body.release) {
-          return req.badRequest('admin:installTheme:Release is required');
-        }
-
-        if (we.view.isThemeInstalled(req.params.name)) {
-          return we.controllers.admin.enableTheme(req, res, next);
-        }
-
-        we.view.downloadAndInstallTheme(req.params.name, req.body.release, (err)=> {
-          if (err) return res.queryError(err);
-
-          const activeThemes = Object.keys(we.view.themes).join('|');
-
-          req.we.plugins['we-plugin-db-system-settings']
-          .setConfigs({
-            activeThemes: activeThemes,
-            siteTheme: req.params.name,
-            [ req.params.name + 'ColorName' ]: (req.body.colorName || 'default')
-          }, (err, updatedSettings)=> {
-            if (err) return res.queryError(err);
-
-            res.addMessage('success', {
-              text: 'admin:installTheme.success'
-            });
-
-            res.locals.data = {
-              updatedSettings: updatedSettings
-            };
-
-            // small timeout to wait system settings lifecircle that will enable this theme in all running instances:
-            setTimeout( ()=> {
-              res.ok();
-            }, 200);
-          });
-        });
-      },
-
-      updateTheme(req, res) {
-        const we = req.we;
-
-        if (!we.systemSettings.themesToUpdate) return res.ok();
-
-        let themesToUpdate = JSON.parse(we.systemSettings.themesToUpdate);
-
-        if (!themesToUpdate[req.params.name]) return res.ok();
-
-        we.view.downloadAndInstallTheme(req.params.name, req.body.release, (err)=> {
-          if (err) return res.queryError(err);
-
-          delete themesToUpdate[req.params.name];
-
-          if (!Object.keys(themesToUpdate).length) {
-            themesToUpdate = null;
-          }
-
-          req.we.plugins['we-plugin-db-system-settings']
-          .setConfigs({
-            themesToUpdate: JSON.stringify(themesToUpdate || {})
-          }, (err)=> {
-            if (err) return res.queryError(err);
-
-            res.addMessage('success', {
-              text: 'admin:updateTheme.success'
-            });
-
-            setTimeout( ()=> {
-              res.ok({
-                themesToUpdate: themesToUpdate
-              });
-            }, 200);
-
-          });
-        });
-      },
-
-      enableTheme(req, res) {
-        const we = req.we;
-
-        if (!req.body.release) {
-          return req.badRequest('admin:installTheme:Release is required');
-        }
-
-        we.view.enableTheme(req.params.name, (err)=> {
-          if (err) return res.queryError(err);
-
-          req.we.plugins['we-plugin-db-system-settings']
-          .setConfigs({
-            siteTheme: req.params.name,
-            [ req.params.name + 'ColorName' ]: (req.body.colorName || 'default')
-          }, (err, updatedSettings)=> {
-            if (err) return res.queryError(err);
-
-            res.addMessage('success', {
-              text: 'admin:enableTheme.success'
-            });
-
-            res.locals.metadata = {
-              updatedSettings: updatedSettings
-            };
-
-            res.ok();
-          });
-        });
       }
     });
 
@@ -332,32 +181,32 @@ module.exports = function loadPlugin(projectPath, Plugin) {
       'titleHandler'  : 'i18n',
       'titleI18n'     : 'theme_manage',
       'name'          : 'theme_manage',
-      'controller'    : 'admin',
+      'controller'    : 'adminTheme',
       'action'        : 'findThemes',
       'template'      : 'admin/theme/index',
-      'permission'    : 'manage_theme'
+      'permission'    : true, // verified in controller
     },
 
     'post /admin/theme/:name/install': {
-      'controller'    : 'admin',
+      'controller'    : 'adminTheme',
       'action'        : 'installTheme',
-      'permission'    : 'manage_theme',
+      'permission'    : true, // verified in controller
       'responseType'      : 'json',
     },
     'post /admin/theme-verify-updates': {
       'controller'    : 'adminTheme',
       'action'        : 'verifyAllThemesUpdate',
-      'permission'    : true, // this is verified in controller
+      'permission'    : true, // verified in controller
       'responseType'      : 'json',
     },
     'post /admin/theme/:name/update': {
-      'controller'    : 'admin',
+      'controller'    : 'adminTheme',
       'action'        : 'updateTheme',
-      'permission'    : 'manage_theme',
-      'responseType'      : 'json',
+      'permission'    : true, // verified in controller
+      'responseType'  : 'json',
     },
     'get /theme': {
-      controller: 'admin',
+      controller: 'adminTheme',
       action: 'getAllThemes',
       responseType: 'json'
     }
--- server/controllers/adminTheme.js
+++ server/controllers/adminTheme.js
@@ -1,11 +1,165 @@
 module.exports = {
-  verifyAllThemesUpdate(req, res) {
-    const aclAccess = req.we.acl.canStatic('manage_theme', req.userRoleNames);
-    const haveKey = (req.body.manageKey == req.we.config.manageKey);
+  findThemes(req, res) {
+    const we = req.we;
 
-    if (!aclAccess || !haveKey) {
-      return res.forbidden();
+    if (!haveAccess(req)) return res.forbidden();
+
+    res.locals.themes = we.view.themes;
+    res.locals.themeConfigs = we.config.themes;
+
+    res.ok();
+  },
+
+  getAllThemes(req, res) {
+    const we = req.we;
+
+    res.locals.themes = we.view.themes;
+    res.locals.themeConfigs = we.config.themes;
+
+    let themes = {};
+
+    for(let name in res.locals.themes) {
+      if (!res.locals.themes[name].description) continue;
+
+      themes[name] = {
+        name: name,
+        imageThumbnail: res.locals.themes[name].imageThumbnail,
+        imageLarge: res.locals.themes[name].imageLarge,
+        description: res.locals.themes[name].description,
+        configs: res.locals.themes[name].configs
+      };
     }
+
+    let firstThemeName;
+    if (res.locals.themes && res.locals.themes[0]) {
+      firstThemeName = res.locals.themes[0].name;
+    }
+
+    res.ok({
+      themes: themes,
+      enabled: we.systemSettings.siteTheme || firstThemeName
+    });
+  },
+
+  installTheme(req, res, next) {
+    const we = req.we;
+
+    if (!haveAccess(req)) return res.forbidden();
+
+    // req.params.name
+    // req.body.colorName
+    // req.body.release
+
+    if (!req.body.release) {
+      return req.badRequest('admin:installTheme:Release is required');
+    }
+
+    if (we.view.isThemeInstalled(req.params.name)) {
+      return we.controllers.admin.enableTheme(req, res, next);
+    }
+
+    we.view.downloadAndInstallTheme(req.params.name, req.body.release, (err)=> {
+      if (err) return res.queryError(err);
+
+      const activeThemes = Object.keys(we.view.themes).join('|');
+
+      req.we.plugins['we-plugin-db-system-settings']
+      .setConfigs({
+        activeThemes: activeThemes,
+        siteTheme: req.params.name,
+        [ req.params.name + 'ColorName' ]: (req.body.colorName || 'default')
+      }, (err, updatedSettings)=> {
+        if (err) return res.queryError(err);
+
+        res.addMessage('success', {
+          text: 'admin:installTheme.success'
+        });
+
+        res.locals.data = {
+          updatedSettings: updatedSettings
+        };
+
+        // small timeout to wait system settings lifecircle that will enable this theme in all running instances:
+        setTimeout( ()=> {
+          res.ok();
+        }, 200);
+      });
+    });
+  },
+
+  updateTheme(req, res) {
+    const we = req.we;
+
+    if (!haveAccess(req)) return res.forbidden();
+
+    if (!we.systemSettings.themesToUpdate) return res.ok();
+
+    let themesToUpdate = JSON.parse(we.systemSettings.themesToUpdate);
+
+    if (!themesToUpdate[req.params.name]) return res.ok();
+
+    we.view.downloadAndInstallTheme(req.params.name, req.body.release, (err)=> {
+      if (err) return res.queryError(err);
+
+      delete themesToUpdate[req.params.name];
+
+      if (!Object.keys(themesToUpdate).length) {
+        themesToUpdate = null;
+      }
+
+      req.we.plugins['we-plugin-db-system-settings']
+      .setConfigs({
+        themesToUpdate: JSON.stringify(themesToUpdate || {})
+      }, (err)=> {
+        if (err) return res.queryError(err);
+
+        res.addMessage('success', {
+          text: 'admin:updateTheme.success'
+        });
+
+        setTimeout( ()=> {
+          res.ok({
+            themesToUpdate: themesToUpdate
+          });
+        }, 200);
+
+      });
+    });
+  },
+
+  enableTheme(req, res) {
+    const we = req.we;
+
+    if (!haveAccess(req)) return res.forbidden();
+
+    if (!req.body.release) {
+      return req.badRequest('admin:installTheme:Release is required');
+    }
+
+    we.view.enableTheme(req.params.name, (err)=> {
+      if (err) return res.queryError(err);
+
+      req.we.plugins['we-plugin-db-system-settings']
+      .setConfigs({
+        siteTheme: req.params.name,
+        [ req.params.name + 'ColorName' ]: (req.body.colorName || 'default')
+      }, (err, updatedSettings)=> {
+        if (err) return res.queryError(err);
+
+        res.addMessage('success', {
+          text: 'admin:enableTheme.success'
+        });
+
+        res.locals.metadata = {
+          updatedSettings: updatedSettings
+        };
+
+        res.ok();
+      });
+    });
+  },
+  verifyAllThemesUpdate(req, res) {
+    if (!haveAccess(req)) return res.forbidden();
 
     req.we.view.update.verifyAllThemesUpdate(req.we, (err, result)=> {
       if (err) return res.queryError(err);
@@ -15,3 +169,14 @@ module.exports = {
     });
   }
 };
+
+function haveAccess(req) {
+  const aclAccess = req.we.acl.canStatic('manage_theme', req.userRoleNames);
+  const haveKey = (req.we.config.manageKey && (req.body.manageKey == req.we.config.manageKey));
+
+  if (aclAccess || haveKey) {
+    return true;
+  }
+
+  return false;
+}
